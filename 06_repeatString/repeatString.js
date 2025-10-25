0df1a9adbcfc4c96e87249bab2d8dc344dfa83ed
Tack-12 <pranayauni1@gmail.com>
2025-10-25 00:33:33 -0500

    Calculator Done

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(string,num) {
 
+    if (num<0){
+        return "ERROR"
+    }
+    
+    let total ="";
+    for(i=0; i<num; i++){
+        total += string;
+    }
+
+    return total;
 };
 
 // Do not edit below this line
--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return (a+b)
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return ((a)-(b))
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let total = arr.reduce(((acc , a)=> {return acc+= a}),0);
+
+  return total
 };
 
-const multiply = function() {
+const multiply = function(arr) {
+
+  total = arr.reduce((acc , a)=> {return acc*= a },1)
+
+  return total;
 
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return (Math.pow(a,b));
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  let total =1;
+	for(i=n ; i>0 ; i--){
+    total *= i
+  }
+  return total;
 };
 
 // Do not edit below this line
