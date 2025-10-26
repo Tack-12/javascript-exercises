9a6e45f38dbe9c7fe95c6b435e3ba926b2bd80c7
Tack-12 <pranayauni1@gmail.com>
2025-10-26 00:45:12 -0500

    COmpleted the remaining Objects part :)

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,6 +1,27 @@
-const findTheOldest = function() {
+const findTheOldest = function (object) {
 
+    let oldest = object.reduce((acc,a)=>{
+        
+        if(getAge(acc.yearOfBirth,acc.yearOfDeath)> getAge(a.yearOfBirth,a.yearOfDeath)){
+            return acc;
+        }
+        else{
+            acc = a;
+            return acc;
+        }
+    },{})
+
+    return(oldest)
 };
+
+function getAge(birth,death){
+    if ( typeof death === "undefined"){
+            death = new Date().getFullYear();
+        }
+    
+    return(death - birth);
+}
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
